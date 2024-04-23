cce8a940c11ed4a077a544c22a9928a875cc567d
sarath <sarathakash@gmail.com>
2024-04-23 20:24:01 +0530

    this is my second commit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Devops and CICD Jenkins Application & webhooks github hooks'
+    return 'Welcome to Devops and CICD Jenkins Application & webhooks github automatic the pull and push in the jenkins hooks'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
